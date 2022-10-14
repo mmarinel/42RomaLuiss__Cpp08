6a6933d33d4860cd87ce0a71913b2c72c1b231b5
Matteo Marinelli <mmarinel@e4r11p4.42roma.it>
2022-10-14 17:46:50 +0200

    ex00: final tests

--- ex00/easyFind.hpp
+++ ex00/easyFind.hpp
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 10:59:42 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/10/11 19:12:20 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:41:51 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #define EASY_FIND_H
 
 # include <iostream>
+# include <stdexcept>
 
 # include "../colors.hpp"
 
 /**
  * @brief this function template returns a const reference to the first occurrence
- * of a given integer in a container of ints. If no elements are found, an exception is thrown.
+ * of a given integer in a container of integers. If no elements are found, an exception is thrown.
  * 
  * @tparam T 
  * @param container 
@@ -29,15 +30,22 @@
 template <typename T>
 int	easyfind( const T& container, int to_find ) {
 	typename T::const_iterator	it;
+	typename T::const_iterator	begin_it;
 	typename T::const_iterator	end_it;
 
-	it = container.cbegin();
+	begin_it = container.cbegin();
 	end_it = container.cend();
 
+	it = begin_it;
 	while (it != end_it) {
 		if (*it == to_find)
 		{
-			std::cout << YELLOW << "occurrence found at pos " << it - container.cbegin() << RESET << std::endl;
+			std::cout
+				<< YELLOW
+				<< "occurrence found at pos "
+				<< it - begin_it
+				<< RESET
+				<< std::endl;
 			return (static_cast<int>(*it));
 		}
 		it++;
