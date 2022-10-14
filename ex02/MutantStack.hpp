4160c956c89298d084f5bedf749d5b9a43dd3406
Matteo Marinelli <mmarinel@e4r11p4.42roma.it>
2022-10-14 20:29:24 +0200

    ex02: removed dead code

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 04:08:47 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/10/14 15:47:24 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/10/14 20:19:46 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,39 +25,7 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		struct	iterator {
-			public:
-				// Constructors
-				iterator();
-				iterator( typename std::deque<T>::iterator it );
-				iterator( const iterator& to_copy );
-				
-				// Destructors
-				~iterator();
-				
-				// Operators
-				const iterator&			operator=( const iterator& to_copy );
-				bool					operator==( const iterator& other ) const;
-				bool					operator!=( const iterator& other ) const;
-				bool					operator>( const iterator& other ) const;
-				bool					operator<( const iterator& other ) const;
-				bool					operator>=( const iterator& other ) const;
-				bool					operator<=( const iterator& other ) const;
-				T&						operator*();
-				T&						operator[]( int offset );
-				iterator				operator+( size_t offset ) const;
-				iterator				operator-( size_t offset ) const;
-				__SIZE_TYPE__			operator-( const iterator& other )  const;
-				iterator&				operator++();
-				iterator				operator++( int placeholder );
-				iterator&				operator--();
-				iterator				operator--( int placeholder );
-				iterator&				operator+=( size_t offset );
-				iterator&				operator-=( size_t offset );
-			
-			private:
-				typename std::deque<T>::iterator	it;
-		};
+		typedef typename std::deque<T>::iterator	iterator;
 		
 		// Constructors
 		MutantStack();
@@ -87,6 +55,5 @@ template <typename T>
 std::ostream& operator<<( std::ostream &stream, const MutantStack<T> &mut_stack );
 
 # include "MutantStack.tpp"
-# include "MutantStack.iterator.tpp"
 
 #endif
